270fb12f19a487f9c046cb51f185d5e51cde6124
OnofriichukDiana <118926179+OnofriichukDiana@users.noreply.github.com>
2023-12-31 10:04:18 +0200

    Update getInfoFromSlug.ts

--- ui/src/helpers/getInfoFromSlug.ts
+++ ui/src/helpers/getInfoFromSlug.ts
@@ -4,7 +4,7 @@ export function getIdFromSlug(slug: string | undefined): number | null {
     return null;
   }
   const parts = slug.split("_");
-  if (parts.length === 2 && !isNaN(Number(parts[0]))) {
+  if (parts.length >= 2 && !isNaN(Number(parts[0]))) {
     return parseInt(parts[0], 10);
   } else {
     console.error("Invalid slug format:", slug);
